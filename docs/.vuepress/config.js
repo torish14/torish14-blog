8e5b5719a160560f1a9c5dc3aae8633b5716ed60
toshihirokato <nanamin.104@icloud.com>
2020-04-17 11:04:00 +0900

    サイドバーの作成

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/buttons' // h1, h2 が表示される項目名になる
+      // 下記の記述だと h1 として表示される項目名を指定できる
+      // ['/buttons', 'ボタン']
+    ]
+  }
+}
