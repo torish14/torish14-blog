2393bac3af07d354dc87f4b8e39ee6fdf4e9c0d5
toshihirokato <nanamin.104@icloud.com>
2020-04-18 02:00:53 +0900

    /styles/main.css の読み込み

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,13 @@
 module.exports = {
+  head: [["link", { rel: "stylesheet", href: "/styles/main.css" }]],
   themeConfig: {
     sidebar: [
-      '/',
-      '/buttons', // h1, h2 が表示される項目名になる
+      "/",
+      "/buttons", // h1, h2 が表示される項目名になる
       // 下記の記述だと h1 として表示される項目名を指定できる
       // ['/buttons', 'ボタン']
-      '/typography',
-      'list'
-    ]
-  }
-}
+      "/typography",
+      "list",
+    ],
+  },
+};
