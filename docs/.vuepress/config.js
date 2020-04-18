27ea0d83a27b5e354d6885651b49cf334602d58b
toshihirokato <nanamin.104@icloud.com>
2020-04-18 17:54:10 +0900

    メインページのタイトル・ナビゲーションバー等の変更

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,51 @@
 module.exports = {
+  plugins: ["@vuepress/blog"],
+  title: "torishのストレングスファインダーブログ",
+  description: "ようこそ！",
+  locales: {
+    "/": {
+      lang: "ja",
+    },
+  },
   base: "/styleguide/",
+  meta: [
+    { charset: "utf-8" },
+    { name: "view-port", content: "width=device-width, initial-scale=1" },
+    { name: "theme-color", content: "#7C4DFF" },
+  ],
   head: [
-    ["link", { rel: "stylesheet", href: "/styles/adjustment-layout.css" }],
-    ["link", { rel: "stylesheet", href: "/styles/main.css" }],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "images/lightbulb-on-outline.png",
+      },
+    ],
+    // ["link", { rel: "stylesheet", href: "/styles/adjustment-layout.css" }],
+    // ["link", { rel: "stylesheet", href: "/styles/main.css" }],
   ],
   themeConfig: {
+    logo: "images/lightbulb-on-outline.png",
+    searchPlaceholder: "Search...",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Buttons", link: "/buttons" },
+      { text: "Typography", link: "/typography" },
+      { text: "List", link: "/list" },
+      {
+        text: "More",
+        items: [
+          { text: "Twitter", link: "https://twitter.com" },
+          { text: "Github", link: "https://github.com" },
+          { text: "Facebook", link: "https://facebook.com" },
+        ],
+      },
+    ],
     sidebar: [
       "/",
-      "/buttons", // h1, h2 が表示される項目名になる
+      // h1, h2 が表示される項目名になる
+      "/buttons",
       // 下記の記述だと h1 として表示される項目名を指定できる
       // ['/buttons', 'ボタン']
       "/typography",
